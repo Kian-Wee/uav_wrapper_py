d5df9ef56f47ce84bf289bc902eac1048a8eba1f
Kian Wee <kianweetan2002@gmail.com>
2023-07-28 16:42:05 +0800

    bugfixes

--- tf_publisher.py
+++ tf_publisher.py
@@ -10,10 +10,25 @@ from geometry_msgs.msg import TransformStamped
 
 class tf_publisher():
 
-        def __init__(self, distance, uav_frame_id="moose",camera_frame_id="camera",target_frame_id="body_setpoint", global_frame_id="map", hover_height=1.2):
+        def __init__(self, distance=0.5, uav_frame_id="moose",camera_frame_id="camera",target_frame_id="body_setpoint", global_frame_id="map", hover_height=1.2):
             self.tfBuffer = tf2_ros.Buffer()
             self.listener = tf2_ros.TransformListener(self.tfBuffer)
             self.uav_to_body_setpoint_broadcaster = tf2_ros.TransformBroadcaster()
+            self.uav_frame_id=uav_frame_id
+            self.camera_frame_id=camera_frame_id
+            self.target_frame_id=target_frame_id
+            self.global_frame_id=global_frame_id
+            self.distance=distance
+            self.hover_height=hover_height
+
+            rosrate=rospy.Rate(60)
+            while not rospy.is_shutdown():
+                self.transform()
+                print("running")
+                rosrate.sleep()
+
+
+        def init_variables(self, distance=0.5, uav_frame_id="moose",camera_frame_id="camera",target_frame_id="body_setpoint", global_frame_id="map", hover_height=1.2):
             self.uav_frame_id=uav_frame_id
             self.camera_frame_id=camera_frame_id
             self.target_frame_id=target_frame_id
@@ -26,34 +41,32 @@ class tf_publisher():
             # If the setpoint exists, calculate new body setpoint 0.4m out of the wall
             try:
 
-                # # No changes needed for rotation
-                # transform_stamped = self.tfBuffer.lookup_transform("camera", "map", rospy.Time(0))
-                # self.camera_setpoint.rx = transform_stamped.transform.rotation.x
-                # self.camera_setpoint.ry = transform_stamped.transform.rotation.y
-                # self.camera_setpoint.rz = transform_stamped.transform.rotation.z
-                # self.camera_setpoint.rw = transform_stamped.transform.rotation.w
 
-                # Find vector from drone to new body setpoint by setting it cameratobody_x away from camera
-                # (trans_s,rot_s)=self.listener.lookupTransform(camera_frame_id, uav_frame_id, rospy.Time(0)) #to frame, from frame, time
-                transform_stamped2 = self.tfBuffer.lookup_transform(self.camera_frame_id, self.uav_frame_id, rospy.Time(0))
-
-
-                # mag = magnitude(np.array(trans_s))
-                # trans_s_new = ( 1 - cameratobody_x/mag) * np.array(trans_s) / mag
-                vector_array = [transform_stamped2.transform.translation.x, transform_stamped2.transform.translation.y, transform_stamped2.transform.translation.z]
-                mag = magnitude(np.array(vector_array))
-                trans_s_new = ( 1 - self.distance/mag) * np.array(vector_array) / mag
-
-                # Send out transform again for visualisation
-                # self.uav_to_body_setpoint_broadcaster.sendTransform(trans_s_new, rot_s, rospy.Time.now(), "body_setpoint", "camera")
                 newtransformStamped = TransformStamped()
                 newtransformStamped.header.frame_id = self.camera_frame_id
                 newtransformStamped.header.stamp = rospy.get_rostime()
                 newtransformStamped.child_frame_id = self.target_frame_id
+
+
+                # Translation: Find vector from drone to new body setpoint by setting it cameratobody_x away from camera
+                transform_stamped2 = self.tfBuffer.lookup_transform(self.camera_frame_id, self.uav_frame_id, rospy.Time(0))
+                vector_array = [transform_stamped2.transform.translation.x, transform_stamped2.transform.translation.y, transform_stamped2.transform.translation.z]
+                mag = magnitude(np.array(vector_array))
+                trans_s_new = (self.distance) * (np.array(vector_array) /mag)
+                # Send out transform again for visualisation
                 newtransformStamped.transform.translation.x = trans_s_new[0]
                 newtransformStamped.transform.translation.y = trans_s_new[1]
                 newtransformStamped.transform.translation.z = trans_s_new[2]
-                newtransformStamped.transform.rotation = transform_stamped2.transform.rotation
+
+
+                # Rotation: No changes needed for rotation
+                transform_stamped = self.tfBuffer.lookup_transform("camera", "map", rospy.Time(0))
+                newtransformStamped.transform.rotation.x  = transform_stamped.transform.rotation.x
+                newtransformStamped.transform.rotation.y  = transform_stamped.transform.rotation.y
+                newtransformStamped.transform.rotation.z  = transform_stamped.transform.rotation.z
+                newtransformStamped.transform.rotation.w  = transform_stamped.transform.rotation.w
+
+
                 self.uav_to_body_setpoint_broadcaster.sendTransform(newtransformStamped)
 
             except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
@@ -61,3 +74,15 @@ class tf_publisher():
 
 def magnitude(vector):
     return math.sqrt(sum(pow(element, 2) for element in vector))
+
+
+
+if __name__ == '__main__':
+    
+    rospy.init_node('tf_publisher_node')
+
+    node = tf_publisher()
+
+
+
+    rospy.spin()
--- wall.py
+++ wall.py
@@ -13,7 +13,7 @@ from sensor_msgs.msg import Range
 import time
 import numpy as np
 import numpy as np
-import tf_publisher
+from tf_publisher import tf_publisher
 import tf2_ros
 
 # Example code for a multi-staged multi-controller wall approach, to get it close to a wall and then slowly jog in 
@@ -25,24 +25,19 @@ rate = 60 # Update rate
 cameratobody_x = 0.5 # +ve is forward
 cameratobody_dist = 0.5 # used for range sensor, should be same as cameratobody_x but set to a higher number to allow for less stringent deployment
 contact_threshold = 0.1 # UAV is assumed to be touching the wall at this distance
-cameratobody_dist = 0.5 # used for range sensor, should be same as cameratobody_x but set to a higher number to allow for less stringent deployment
-contact_threshold = 0.1 # UAV is assumed to be touching the wall at this distance
 
 # Camera Topic for desired setpoint
-camera_setpoint_topic="/tf"
-camera_frame_id="/camera"
-uav_frame_id="/moose"
-uav_frame_id="/moose"
-world_frame_id="/map"
+camera_setpoint_topic="tf"
+camera_frame_id="camera"
+uav_frame_id="moose"
+world_frame_id="map"
 
 # Threshold for jogging, when setpoint is under these conditions, drone will jog instead
-threshold_jog=0.7 #m
 threshold_jog=0.7 #m
 threshold_jog_deg=5 #deg
 # Rear Thruster Topic
 thruster_output_topic="/thruster/pwm"
 max_deployment_times = 1
-hover_height=1.2
 hover_height=1.2
 
 ser = serial.Serial('/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_58:CF:79:02:98:E4-if00', 115200) #ls /dev/serial/by-id/*
@@ -62,9 +57,6 @@ class offboard_node():
         print("Using TF Transforms for setpoints")
         self.listener = tf.TransformListener()
 
-        print("Using TF Transforms for setpoints")
-        self.listener = tf.TransformListener()
-
         self.last_acceptable_setpoint = uav_variables()
         self.last_acceptable_setpoint = self.camera_setpoint
         
@@ -80,28 +72,33 @@ class offboard_node():
 
         deployment_times = 0
 
-        tf_pub = tf_publisher(distance=cameratobody_x,hover_height=hover_height)
-        
+        # self.tf_pub = tf_publisher()
+        # self.tf_pub.init_variables(cameratobody_x,hover_height=hover_height)
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
 
         self.rosrate=rospy.Rate(rate)
         rospy.on_shutdown(self.quit)
     
         while not rospy.is_shutdown():
 
-            tf_pub.transform()
+            # self.tf_pub.transform()
             try:
                 # TODO , THIS DOESNT WORK WITHIN THE SAME NODE 
                 # Look at the final transform to body_setpoint for final setpoint
-                transform_stamped = self.tfBuffer.lookup_transform("body_setpoint", "map", rospy.Time(0), rospy.Duration(0.1))
-                print(transform_stamped)
-                print("bye")
+                # print("hihi")
+                # transform_stamped = self.tfBuffer.lookup_transform("body_setpoint", "map", rospy.Time(0), rospy.Duration(0.1))
+                transform_stamped = self.tfBuffer.lookup_transform(world_frame_id, "body_setpoint", rospy.Time(0))
+                # print("byebye")
+                # rospy.loginfo_throttle(1, str(transform_stamped))
                 self.camera_setpoint.x = transform_stamped.transform.translation.x
                 self.camera_setpoint.y = transform_stamped.transform.translation.y
-                self.camera_setpoint.z = self.hover_height
+                self.camera_setpoint.z = hover_height
                 self.camera_setpoint.rx = transform_stamped.transform.rotation.x
                 self.camera_setpoint.ry = transform_stamped.transform.rotation.y
                 self.camera_setpoint.rz = transform_stamped.transform.rotation.z
                 self.camera_setpoint.rw = transform_stamped.transform.rotation.w
+                # print("read %s", transform_stamped.transform.rotation.z)
 
             except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
                 rospy.logdebug("Missing body setpoint tf transform")
@@ -109,14 +106,13 @@ class offboard_node():
 
             current_yaw=euler.quat2euler([self.uav.pos.rw,self.uav.pos.rx,self.uav.pos.ry,self.uav.pos.rz])[2] #wxyz default
             setpoint_yaw=euler.quat2euler([self.camera_setpoint.rw,self.camera_setpoint.rx,self.camera_setpoint.ry,self.camera_setpoint.rz])[2] #wxyz default
+            # print("test",abs(self.camera_setpoint.x - self.uav.pos.x) < threshold_jog,abs(self.camera_setpoint.y-self.uav.pos.y) < threshold_jog,abs(self.camera_setpoint.z-self.uav.pos.z) < threshold_jog)
 
             # No setpoint sent yet
             if self.camera_setpoint.x == 0 and self.camera_setpoint.y ==0 and self.camera_setpoint.z ==0:
                 rospy.loginfo_throttle_identical(2, "Missing setpoint/tf, hovering at current location")
-                rospy.loginfo_throttle_identical(2, "Missing setpoint/tf, hovering at current location")
                 self.uav.setpoint_quat(self.uav.pos.x,self.uav.pos.y,self.uav.pos.z,self.uav.pos.rx,self.uav.pos.ry,self.uav.pos.rz,self.uav.pos.rw) #callback local position
-            elif abs(self.camera_setpoint.x - self.uav.pos.x) < threshold_jog and abs(self.camera_setpoint.y-self.uav.pos.y) < threshold_jog and abs(self.camera_setpoint.z-self.uav.pos.z) < threshold_jog:
-                rospy.loginfo_throttle_identical(3,"Setpoint[%s,%s,%s] close to drone, jogging it inwards based on past position",self.last_acceptable_setpoint.x,self.last_acceptable_setpoint.y,self.last_acceptable_setpoint.z)
+            elif abs(self.camera_setpoint.x - self.uav.pos.x) < threshold_jog and abs(self.camera_setpoint.y-self.uav.pos.y) < threshold_jog:  #and abs(self.camera_setpoint.z-self.uav.pos.z) < threshold_jog
                 rospy.loginfo_throttle_identical(3,"Setpoint[%s,%s,%s] close to drone, jogging it inwards based on past position",self.last_acceptable_setpoint.x,self.last_acceptable_setpoint.y,self.last_acceptable_setpoint.z)
                  # Stop and yaw on the spot with less agressive nearfield controller when close to wall
                 if degrees(abs(setpoint_yaw-current_yaw)) > threshold_jog_deg:
@@ -125,25 +121,22 @@ class offboard_node():
                     self.yaw_setpoint.x=self.uav.pos.x
                     self.yaw_setpoint.y=self.uav.pos.y
                     self.yaw_setpoint.z=hover_height
-                    self.yaw_setpoint.z=hover_height
                     self.yaw_setpoint.rx=self.camera_setpoint.rx
                     self.yaw_setpoint.ry=self.camera_setpoint.ry
                     self.yaw_setpoint.rz=self.camera_setpoint.rz
                     self.yaw_setpoint.rw=self.camera_setpoint.rw
                     self.uav.setpoint_controller(self.yaw_setpoint,"close")
-                    print("yaw")
                # Switch to less aggressive nearfield controller when close to wall and start translating
                 elif deployment_times <max_deployment_times:
                     self.uav.setpoint_controller(self.camera_setpoint,"close")
                     
-                    
                     for i in self.uav.controller_array:
                         if i.name == "aux":
                             thr_val = i.custom_single_controller(self.wall_dist,self.wall_dist)[0]
-                    
-                    rospy.loginfo_throttle_identical(1,"Yaw within margin. Moving with rear thruster @ [%s]", str(translate(thr_val, 0, aux_kp, 0, 50)))
-                    rospy.loginfo_throttle_identical(1,"Yaw within margin. Moving with rear thruster @ [%s]", str(translate(thr_val, 0, aux_kp, 0, 50)))
                     ser.write(str.encode(str(translate(thr_val, 0, aux_kp, 0, 100))))
+
+                    rospy.loginfo_throttle_identical(5,"Yaw within margin. Moving with rear thruster @ [%s]", str(translate(thr_val, 0, aux_kp, 0, 50)))
+
                     if self.wall_dist <= contact_threshold and self.release_stage=="disarmed":
                         rospy.loginfo_throttle_identical(1,"Approached wall, stabilising")
                     if self.wall_dist <= contact_threshold and self.release_stage=="disarmed":
@@ -153,20 +146,14 @@ class offboard_node():
                         self.wall_timer=rospy.get_time()
                     if (self.wall_dist <= contact_threshold and self.release_stage=="contact" and time.time()>=self.wall_timer+self.wall_dur):
                         rospy.loginfo_throttle_identical(1,"Touched wall and stabalised, releasing adhesive")
-                    if (self.wall_dist <= contact_threshold and self.release_stage=="contact" and time.time()>=self.wall_timer+self.wall_dur):
-                        rospy.loginfo_throttle_identical(1,"Touched wall and stabalised, releasing adhesive")
                         self.release_stage= "glue_release"
                         ser.write(str.encode(self.release_stage))
                         self.adh_timer=rospy.get_time()
                     if (self.wall_dist <= contact_threshold and self.release_stage=="glue_release" and time.time()>=self.adh_timer+self.adh_dur):
                         rospy.loginfo_throttle_identical(1,"Dropping payload")
-                    if (self.wall_dist <= contact_threshold and self.release_stage=="glue_release" and time.time()>=self.adh_timer+self.adh_dur):
-                        rospy.loginfo_throttle_identical(1,"Dropping payload")
                         self.release_stage="payload_drop"
                         ser.write(str.encode(self.release_stage))
                         self.reset_timer=rospy.get_time()
-                    if (self.wall_dist <= contact_threshold and self.release_stage=="payload_drop" and time.time()>=self.reset_timer+self.reset_dur):
-                        rospy.loginfo_throttle_identical(1,"Disarming")
                     if (self.wall_dist <= contact_threshold and self.release_stage=="payload_drop" and time.time()>=self.reset_timer+self.reset_dur):
                         rospy.loginfo_throttle_identical(1,"Disarming")
                         self.release_stage="uv_off"
@@ -183,7 +170,7 @@ class offboard_node():
             # Approach setpoint with aggressive controller when far 
             else:
                 rospy.loginfo_throttle_identical(2,"Setpoint far from drone, using controller %s",self.uav.pos.x)
-                rospy.loginfo_throttle_identical(2,"Setpoint far from drone, using controller %s",self.uav.pos.x)
+                # print(self.camera_setpoint.rx)
                 self.last_acceptable_setpoint = self.camera_setpoint
                 self.uav.setpoint_controller(self.camera_setpoint,"far")
 
@@ -192,8 +179,7 @@ class offboard_node():
 
     def range_callback(self, msg):
         self.wall_dist = msg.range - cameratobody_dist
-        self.wall_dist = msg.range - cameratobody_dist
-
+        # print(self.wall_dist)
     
     
     def quit(self):
