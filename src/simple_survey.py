3a76803ed2bccd5946fd7eacbf7d56ebc213d19c
Kian Wee <kianweetan2002@gmail.com>
2023-08-31 20:22:44 +0800

    misc tidying up code

--- src/simple_survey.py
+++ src/simple_survey.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from uav import uav, uav_variables
+from uav import uav
 
 # Example code for a simple survey pattern
 
@@ -12,7 +12,7 @@ class offboard_node():
     def __init__(self):
         print("Initalising Simple Survey Node")
 
-        self.uav = uav(survey_array_z=[[0.9,-1.9,1],[4.79,-1.9,1]]) # Initalise UAV object
+        self.uav = uav(survey_array_z=[[0.9,-1.9,1.2],[4.79,-1.9,1.2]]) # Initalise UAV object
 
         self.rosrate=rospy.Rate(rate)
         rospy.on_shutdown(self.quit)
@@ -22,7 +22,6 @@ class offboard_node():
             self.uav.continous_survey(threshold=0.25)
             self.rosrate.sleep()
 
-    
     def quit(self):
         print("Killing node")
         rospy.signal_shutdown("Node shutting down")
